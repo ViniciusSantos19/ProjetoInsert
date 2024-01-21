d5e8085bd7c6559c91305eafa13a298a0b7b0d16
ViniciusSantos19 <vinicontatosq@gmail.com>
2024-01-20 23:05:02 -0300

    adicionei um comentário apenas para testar a chave shh

--- internal/db/queries.go
+++ internal/db/queries.go
@@ -138,6 +138,6 @@ func InsertCheckinsInBatches(db *sql.DB, checkins <-chan model.Checkin) error {
 	}
 
 	fmt.Printf("%d batches inserted\n", batchCount)
-
+	fmt.Println("apenas um teste para a chave shh")
 	return nil
 }
